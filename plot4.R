82f97dcacdb98d6869eacf52790608e2bc876ddd
Bhael <blsingh@ucdavis.edu>
2016-11-07 11:37:49 -0800

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -1,4 +1,9 @@
+wk1d <- read.table("household_power_consumption.txt", sep=";",header=T)
 
+data <- wk1d[wk1d$Date=='1/2/2007'| wk1d$Date=='2/2/2007',]
+
+data$dt<-paste(data$Date,data$Time)
+data$datetime<-strptime(data$dt,"%d/%m/%Y %H:%M:%S")
 
 par(mfcol=c(2,2))
 with(data, {
@@ -7,4 +12,6 @@ with(data, {
   lines(data$datetime,as.numeric(as.character(data$Sub_metering_2)), col="red")
   lines(data$datetime,as.numeric(as.character(data$Sub_metering_3)), col="green")
   plot(data$datetime,as.numeric(as.character(data$Voltage)), type='l', xlab="datetime",ylab="voltage")
+  plot(data$datetime,as.numeric(as.character(data$Global_reactive_power)), type='l', xlab="datetime",ylab="Global reactive power")
 })
+dev.copy(png, file="Plot4.png")
